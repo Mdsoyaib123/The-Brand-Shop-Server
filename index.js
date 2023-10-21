b2e9aa43d573e8ad9af29dbffabfc938933eae18
Mdsoyaib123 <mdsoyaibsourav@gmail.com>
2023-10-21 20:26:43 +0600

    added the file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId, HostAddress } = require('mongodb');
 const { config } = require('dotenv');
 
@@ -14,8 +15,11 @@ app.use(cors());
 app.use(express.json());
 
 // console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASS);
 
-const uri = "mongodb+srv://mdsoyaibsourav:SHwEM3yQAPpIGML1@cluster0.hzg7hl2.mongodb.net/?retryWrites=true&w=majority";
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hzg7hl2.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://mdsoyaibsourav:SHwEM3yQAPpIGML1@cluster0.hzg7hl2.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 
